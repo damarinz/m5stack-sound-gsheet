943266f7439ae362feb6f015c49e6224ef13f832
damarinz <kaMab0ko39>
2019-11-10 22:23:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# 使い方
+
+
+1. 新しいGoogle sheetを作成します
+2. シート名を適宜変更します
+3. 1行目のA列に datetime, B列に sound と入力します
+4. ツール＞スクリプトエディタを選択し、M5Stack-sound-googlespreadsheet.gasをペーストします。
+5. SHEET_NAMEのプロパティを利用しているので、プロジェクトのプロパティで、ユーザー設定プロパティ値を設定します。SHEET_NAME: sound
+
+![](images/GAS-ProjectProperty.png)
+
+6. デプロイします。doPostを叩くため、"Anyone, even anonymous"を選びます
+
+![](images/GAS-DeployOption.png)
+
+6. PAWなどからjson形式で叩いてみます。bodyにjsonを選んで、soundのkeyと任意のvalueを記入します
+
+302が出たら、シート側を見ると値が書かれてるはずです。
+
+![](images/TestFromPaw.png)
+
+7. M5Stack FireをつないだArduino IDEに、m5stack-sound-https-post-gsheet.inoをセットします。
+WiFiの情報を書く部分があるので、記入します。11aにはM5Stackが対応してないので、2.4GHz帯の11nなどを選んでください
+
+8. 書き込みが成功すれば、M5Stack Fire Analog 34pinの数値が10秒おきにgoogle sheetに書かれます。
---
+++ m5stack-sound-https-post-gsheet.ino
@@ -0,0 +1,80 @@
+#include <WiFi.h>
+#include <M5Stack.h>
+#include <HTTPClient.h>
+
+
+
+#define M5STACKFIRE_MICROPHONE_PIN 34
+
+const char* ssid     = "SSID-11g";   // your network SSID (name of wifi network)
+const char* password = "xxxxxxxxx";    // your network password
+
+//setup start
+void setup() {
+  M5.begin();
+  Wire.begin();
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(100);
+
+
+  Serial.print("Attempting to connect to SSID: ");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // wait 1 second for re-trying
+    delay(1000);
+  }
+
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int micValue = analogRead(M5STACKFIRE_MICROPHONE_PIN);
+
+  Serial.println(micValue);
+
+//generate JSON
+  char json[100];
+  sprintf(json, "{\"sound\": %d }", micValue);
+
+//HTTPClient code start
+  HTTPClient http;
+
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  http.begin("https://script.google.com/macros/s/AKfycbz6TFqd3plX_vrxqY4fXxCADSPKJuSpx3Fn1DBRKoijMvEtYxM/exec"); //HTTP
+
+  Serial.print("[HTTP] POST...\n");
+  // start connection and send HTTP header
+  int httpCode = http.POST(json);
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+
+
+  delay(10000);
+
+}
